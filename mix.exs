fc2f2575b997d51e0c91198f7477633a7e52d145
Archaic Archon <archaicarchon@gmail.com>
2015-09-26 18:12:36 -0400

    Fixed the last commit, should work now!

---
+++ mix.exs
@@ -0,0 +1,33 @@
+defmodule Elmud.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :elmud,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     escript: [main_module: Elmud],
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    [{:amnesia, github: "meh/amnesia", tag: :master}]
+  end
+end
